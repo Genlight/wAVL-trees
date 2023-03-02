b67846d9ea04106abf65b682c4e1fcaceb8b6e33
Alexander Genser <genlight@heartgaming.at>
2023-03-02 19:33:41 +0100

    REMOVED: some test files UPDATED: nikis suggestion for test_RTick.hs

--- src/test_RTick.hs
+++ src/test_RTick.hs
@@ -4,7 +4,7 @@
 
 module Test_Rtick where
 
-import Prelude hiding (pure, ap)
+import Prelude hiding (pure, ap, (<*>))
 import Language.Haskell.Liquid.RTick as RTick
 
 {-@ data Tree [rk] a = Nil | Tree { val :: a, 
@@ -42,6 +42,7 @@ notEmptyTree :: Tree a -> Bool
 notEmptyTree Nil = False
 notEmptyTree _ = True
 
+-- this is valid, but uses a workaround by declaring a another Tree b, which i check to be equal to r' and then don't use b
 {-@ tickWrapper :: x:a -> {n:Int | n >= 0} -> l:Tree a -> b:Tree a -> r:Tick ({r':Tree a |  b == r'}) -> {t:Tick ({t':Tree a | val t' == x && rk t' == n && left t' == l && b == right t'}) | tcost t == tcost r } @-}
 tickWrapper :: a -> Int -> Tree a -> Tree a -> Tick (Tree a) -> Tick (Tree a)
 tickWrapper x n l _ r = (pure tree) `ap` (pure x) `ap` (pure n) `ap` (pure l) `ap` r
@@ -50,11 +51,22 @@ tickWrapper x n l _ r = (pure tree) `ap` (pure x) `ap` (pure n) `ap` (pure l) `a
 tree :: a -> Int -> Tree a -> Tree a -> Tree a
 tree x n l r = Tree x n l r 
 
--- Test
-main = do
-    mapM_ print [a,b,c]
-  where
-    a = Tree 8 0 Nil Nil
-    b = Tree 3 0 Nil Nil
-    c = Tree 5 2 a b
-    -- d = tval (tickWrapper 5 2 a b (pure b))
+-- Defined in RTick library that can open the Tick
+exact :: Tick a -> Tick a
+{-@ exact :: t:Tick a -> {to:Tick ({v: a | v == (tval t)})| to = t} @-}
+exact (Tick c v) = Tick c v 
+
+{-@ exactWAVL :: v:Tick (v':Wavl) -> t:Tick (t':Wavl) @-}
+exactWAVL :: Tick (Tree a) -> Tick (Tree a)
+exactWAVL t = exact t
+
+{-@ useTW :: x:a -> {n:Int | n >= 0 } -> l:Tree a -> r:Tick (r':Tree a) -> {t:Tick ({t':Tree a | val t' == x && rk t' == n && left t' == l }) | tcost t == tcost r } @-}
+useTW :: a -> Int -> Tree a -> Tick (Tree a) -> Tick (Tree a)
+useTW x n l tt = tickWrapper x n l (tval tt) (exact tt)
+
+-- should be also be valid, but isn't
+-- {-@ tickWrapper' :: x:a -> {n:Int | n >= 0} -> l:Tree a -> b:Tree a -> r:Tick ({r':Tree a |  b == r'}) -> {t:Tick ({t':Tree a | val t' == x && rk t' == n && left t' == l && b == right t'}) | tcost t == tcost r } @-}
+-- tickWrapper' :: a -> Int -> Tree a -> Tree a -> Tick (Tree a) -> Tick (Tree a)
+-- tickWrapper' x n l _ r = Tree $ (pure x) <*> (pure n) <*> (pure l) <*> r
+
+
