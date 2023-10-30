679de37752171689f3f1d9b923d8f231a6f6abd2
Alexander Genser <genlight@heartgaming.at>
2023-10-30 10:29:03 +0100

    wip

--- src/RBTree.hs
+++ src/RBTree.hs
@@ -51,7 +51,7 @@ data Col = R | B deriving (Eq, Show)
 
 {-@ data Tree [ht1] a = Nil | Tree { val :: a, 
                                     rd :: Col,
-                                    ht1 :: v:Nat,
+                                    ht1 :: v:Pos,
                                     left :: ChildT a ht1,
                                     right :: ChildT a ht1 } @-} 
 data Tree a = Nil | Tree { val :: a, rd :: Col, ht1:: Int, left :: (Tree a), right :: (Tree a)} deriving Show
@@ -59,6 +59,7 @@ data Tree a = Nil | Tree { val :: a, rd :: Col, ht1:: Int, left :: (Tree a), rig
 
 {-@ type ChildT a T = {v:Tree a | ht v + 1 <= T } @-}
 {-@ type Nat = {v:Int | v >= 0} @-}
+{-@ type Pos = {v:Int | v >= 1} @-}
 
 {-@ measure potT @-}
 {-@ potT :: t:Tree a -> Nat / [ht t] @-}
@@ -92,7 +93,7 @@ test t@(Tree x c h l r)
     | empty l && empty r && c == R = Tree x c h l r -- t is leaf, no cost 
     | otherwise = check (rk r) (Tree x c h l (test r)) -- do the checking which changes colours to red if a change happened in r
 
-{-@ check :: Col -> t:Tree a -> {v:Tree a | ht t == ht v} @-}
+{-@ check :: Col -> t:Tree a -> {v:Tree a | ht t == ht v && ht(right v) == ht (right t)} @-}
 check :: Col -> Tree a -> Tree a
 check c Nil = Nil
 check c t@(Tree a b h l r) 
@@ -101,44 +102,57 @@ check c t@(Tree a b h l r)
     | otherwise = red t  -- change B to R, incur cost of 1 and pot - 1
 
  -- do the checking which changes colours to red if a change happened in r
-{-@ testT :: t:Tree a -> v:Tick (v':Tree a) @-}
+{-@ testT :: {t:Tree a | not empty t && ht (right t) < ht t} 
+                    -> {v:Tick (
+                        {v':Tree a | ht (right v') == ht (right t) && ht v' == ht t}) | 
+                        (ht (right (tval v)) < ht (tval v)) && 
+                        ht (tval v) == ht t && 
+                        ht (right(tval v)) == ht (right t)} 
+                    / [ht t] @-}
 testT :: Tree a -> Tick (Tree a)
-testT Nil = RTick.return Nil
+-- testT Nil = RTick.return Nil
 testT t@(Tree x c h l r)
     | empty l && empty r && c == B = RTick.wait (red t) -- t is leaf, cost of 1 is incurred, and pot - 1
     | empty l && empty r && c == R = RTick.return (Tree x c h l r) -- t is leaf, no cost 
-    | otherwise = checkT (rk r) (liftM5 Tree (pure x) (pure c) (pure h) (pure l) (testT r))
+    | empty r = RTick.return (Tree x c h l r) -- t is leaf, no cost 
+    | otherwise = Tick (tcost (testT r)) (check (rk r) (Tree x c h l (tval (testT r))))
 
-{-@ checkT :: Col -> t:Tick({t':Tree a | not empty t'}) -> {v:Tick (v':Tree a) | potT (tval v) + tcost v <= potT (tval t) + tcost t + 2 } @-}
-checkT :: Col -> Tick(Tree a) -> Tick (Tree a)
+-- {-@ help :: {t:Tree a | not (empty t) } -> {v:Tick ({v': Tree a | not empty v' && ht t == ht v' && ht (right v') < ht v'}) | ht (right (tval v)) < ht (tval v) } @-}
+-- help :: Tree a -> Tick (Tree a)
+-- help (Tree x c h l r) = Tick (tcost (testT r)) (Tree x c h l (tval (testT r)))
+
+--  | potT (tval v) + tcost v <= potT t + 2 
+{-@ checkT :: Col -> t:Tree a -> v:Tick (v':Tree a) @-}
+checkT :: Col -> Tree a -> Tick (Tree a)
+checkT _ Nil = pure Nil 
 checkT c t 
-    | rk (right ( tval t)) /= c && not (isBlack t) = RTick.step 2 t 
-    | rk (right (tval t)) /= c && rk (tval t) == B = RTick.wmap red t
-    | otherwise = t
+    | rk (right t) /= c && not (rk t == B) = RTick.step 2 (pure t) 
+    | rk (right t) /= c && rk (t) == B = RTick.wmap red (pure t)
+    | otherwise = pure t
 
-{-@ red :: {t:Tree a | not empty t && rk t == B} -> {v:Tree a | rk v == R && potT t == potT v + 1 && ht t == ht v } @-}
+{-@ red :: {t:Tree a | not empty t && rk t == B } -> {v:Tree a | rk v == R && potT t == potT v + 1 && ht t == ht v && right t == right v } @-}
 red :: Tree a -> Tree a
 red t@(Tree a _ h l r) = Tree a R h l r
 
-{-@ inline isBlack @-}
-isBlack :: Tick (Tree a) -> Bool
-isBlack t = rk (tval t) == B 
+-- {-@ inline isBlack @-}
+-- isBlack :: Tick (Tree a) -> Bool
+-- isBlack t = rk (tval t) == B 
 
-height :: Tree a -> Tree a -> Int
-height l r = if (ht l) > (ht r) then (1 + ht l) else (1 + ht r)
+-- height :: Tree a -> Tree a -> Int
+-- height l r = if (ht l) > (ht r) then (1 + ht l) else (1 + ht r)
 
--- copied from my implementation in RTick, s. commit 
-{-@ (<$>) :: f:(a -> b) -> t1:Tick a
-                    -> { t:Tick b | Tick (tcost t1) (f (tval t1)) == t }
-@-}
-infixl 4 <$>
-(<$>) :: (a -> b) -> Tick a -> Tick b
-(<$>) = RTick.fmap 
--- f <$> a <*> b <*> c = Tick (a1 + a2)
+-- -- copied from my implementation in RTick, s. commit 
+-- {-@ (<$>) :: f:(a -> b) -> t1:Tick a
+--                     -> { t:Tick b | Tick (tcost t1) (f (tval t1)) == t }
+-- @-}
+-- infixl 4 <$>
+-- (<$>) :: (a -> b) -> Tick a -> Tick b
+-- (<$>) = RTick.fmap 
+-- -- f <$> a <*> b <*> c = Tick (a1 + a2)
 
-{-@ reflect liftM5 @-}
-{-@ liftM5 :: g:(a -> b -> c -> d -> e -> f) -> t1:Tick a -> t2:Tick b -> t3:Tick c -> t4:Tick d -> t5:Tick e
-                    -> { t:Tick f | g (tval t1) (tval t2) (tval t3) (tval t4) (tval t5) == tval t &&
-                      tcost t1 + tcost t2 + tcost t3 + tcost t4 + tcost t5 == tcost t } @-}
-liftM5 :: (a -> b -> c -> d -> e -> f) -> Tick a -> Tick b -> Tick c -> Tick d -> Tick e -> Tick f
-liftM5 f (Tick m x) (Tick n y) (Tick n1 y1) (Tick n2 y2) (Tick n3 y3) = Tick (m + n + n1 + n2 + n3) (f x y y1 y2 y3)
+-- {-@ reflect liftM5 @-}
+-- {-@ liftM5 :: g:(a -> b -> c -> d -> e -> f) -> t1:Tick a -> t2:Tick b -> t3:Tick c -> t4:Tick d -> t5:Tick e
+--                     -> { t:Tick f | g (tval t1) (tval t2) (tval t3) (tval t4) (tval t5) == tval t &&
+--                       tcost t1 + tcost t2 + tcost t3 + tcost t4 + tcost t5 == tcost t } @-}
+-- liftM5 :: (a -> b -> c -> d -> e -> f) -> Tick a -> Tick b -> Tick c -> Tick d -> Tick e -> Tick f
+-- liftM5 f (Tick m x) (Tick n y) (Tick n1 y1) (Tick n2 y2) (Tick n3 y3) = Tick (m + n + n1 + n2 + n3) (f x y y1 y2 y3)
