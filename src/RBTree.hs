1b9232db7e327e3d4b47d0f9a0331b469d849321
Alexander Genser <genlight@heartgaming.at>
2023-11-02 18:51:22 +0100

    REMOVED: comments from RBTree

--- src/RBTree.hs
+++ src/RBTree.hs
@@ -65,33 +65,6 @@ data Tree a = Nil | Tree { val :: a, rd :: Col, ht1:: Int, left :: (Tree a), rig
 {-@ type EqT T = {v:EqTe T | not empty v } @-}
 {-@ type EqTe T = {v:Tree a | ht1 v == ht1 T && left v == left T && ht (right v) == ht (right T) && left (right v) == left (right T) && ht T == ht v} @-}
 
-{-@ type EqTes X H L R = {v:Tree a | ht1 v == H && left v == L && ht (right v) == ht (tval R) && left (right v) == left (tval R) } @-}
-
-{-@ type EqCT T = {v:EqT T | rk T == rk v} @-}
-
-{-@ type EqTeT X H L R = {v:Tick (v':EqTes X H L R) | ht1 (tval v) == H && left v == L && ht (right (tval v)) == ht (tval R) && left (right (tval v)) == left (tval R) } @-}
-{-@ type EqTeTn X H L R = {v:EqTeT X H L R | not empty (tval v)} @-}
--- EqTs: we expect R :: Tick(Tree a)
-
-{-@ type EqTs X H L R = {v:NETree | ht1 v == H && ht1 v == ht v && val v == X && left v == L && right v == tval R} @-}
-{-@ type EqTsc X C H L R = {v:EqTs X H L R | rk v == C} @-}
-
-{-@ type TTreeCol X C H L R = {v:Tick ({t:Tree | rk t == C && ht t == H && left t == L && right t == (tval R) }) | tcost v == tcost r } @-}
-    --{v:EqTeTn X H L R | rk (tval v) == C  } @-} 
-
--- && right (tval v) == (tval R)
--- {-@ type EqTTs X H L R = {v:Tick (v':EqTs X H L R) | 
---                     ht1 (tval v) == H && left (tval v) == L && ht1 (tval v) == ht (tval v)
---                     && right (tval v) == tval R 
---                     && tcost R == tcost v } @-}
--- {-@ type EqTTsa X H L R = {v:EqTTs X H L R | potT (tval v) + tcost v <= potT T + 2} @-}
-
--- {-@ type  ... = {v:Tick ({t:EqTe }) | tcost R <= tcost v && tcost v <= tcost R + 2 potT (tval v) + tcost v <= potT T + 3} @-}
-{-@ type EqTT T = Tick (v:EqT T) @-}
-
-{-@ type EqTTa T = {v:EqTT T | potT (tval v) + tcost v <= potT T + 3} @-}
-{-@ type EqCTT T = Tick (v:EqCT T) @-}
-
 {-@ type TT1 T = v:Tick ({v':NETree | left v' == left T && ht v' == ht T}) @-}
 {-@ type TT2 T = {v:Tick ({v':NETree | left v' == left (tval T) && ht v' == ht (tval T)}) | left (tval v) == left (tval T)} @-}
 {-@ type TT3 X H L R = {v:Tick ({t:NETree | ht t == H && left t == L && ht (right t) == ht (tval R) }) | tcost v <= tcost r + 2 } @-}
@@ -145,7 +118,7 @@ check c t@(Tree a b h l r)
 -- do the checking which changes colours to red if a change happened in r
 {-@ testT :: t:NETree -> {v:TT1 t | 
                     ((rk t /= rk (tval v)) || (pot1 v + tcost v <= potT t + 2)) || 
-                    ((rk t == rk (tval v)) || (pot1 v + tcost v <= potT t)) } / [ht t] @-} -- EqTTa does not hold
+                    ((rk t == rk (tval v)) || (pot1 v + tcost v <= potT t)) } / [ht t] @-}
 testT :: Tree a -> Tick (Tree a)
 testT t@(Tree x c h l Nil) 
     | c == B = RTick.wait (red t) -- t is leaf, amortised cost of 0
@@ -154,16 +127,9 @@ testT t@(Tree x c h l r) = checkT (rk r) x c h l r'
     where
         r' = testT r
         t' = treeR x c h l r'
-        -- v = (ht r == ht (tval r')) ?= checkT (rk r) x c h l r'
 
--- {-@ help :: t:NETreeR -> v:EqTT a t / [ht t] @-}
--- help :: Tree a -> Tick (Tree a)
--- help t@(Tree x c h l r) = RTick.step (tcost (r')) (checkT (rk r) (Tree x c h l (tval (r'))))
---     where r' = testT r
-
---  | potT (tval v) + tcost v <= potT t + 2 
 {-@ checkT :: Col -> x:a -> c:Col -> h:Pos -> l:ChildT a h -> r:Tick (r':ChildT a h) 
-                    -> v:TT3 x h l r @-} -- EqTTa
+                    -> v:TT3 x h l r @-} 
 checkT :: Col -> a -> Col -> Int -> Tree a -> Tick(Tree a) -> Tick( Tree a)
 -- checkT _ Nil = pure Nil 
 checkT z x c h l r
@@ -171,23 +137,11 @@ checkT z x c h l r
     | rk r' /= z             = RTick.wmap red t
     | rk r' == z = t
         where 
-            t = treeR x c h l r
+            t  = treeR x c h l r
             r' = tval r
-
--- {-@ checkT' :: Col -> t:Tick (t':Tree a) 
---                     -> {v:TT2 t | tcost v <= tcost t + 2 } @-}
--- checkT' :: Col -> Tick(Tree a) -> Tick(Tree a)
--- -- checkT _ Nil = pure Nil 
--- checkT' z t
---     | rk r /= z && c == R   = RTick.step 2 t
---     | rk r' /= z             = RTick.wmap red t
---     | rk r' == z = t
---         where 
---             r' = right (tval t)
---             c = col (tval t)
-    
+   
 -- TRUSTED CODE, we assume that the costs are given from the child to the parent
-{-@ treeR :: x:a -> c:Col -> h:Pos -> l:ChildT a h -> r:Tick (r':ChildT a h) -> {v:Tick ({t:NETree | left t == l && right t == tval r && ht t == h && rk t == c}) | tcost r == tcost v } @-} -- TTreeCol x c h l r
+{-@ treeR :: x:a -> c:Col -> h:Pos -> l:ChildT a h -> r:Tick (r':ChildT a h) -> {v:Tick ({t:NETree | left t == l && right t == tval r && ht t == h && rk t == c}) | tcost r == tcost v } @-}
 treeR :: a -> Col -> Int -> Tree a -> Tick(Tree a) -> Tick(Tree a)
 treeR x c h l r = Tick (tcost r) (Tree x c h l (tval r))
 
@@ -204,27 +158,3 @@ x ?? y = y
 {-@ (?=) :: {y:Bool | y == True } -> z:a -> {v:a | z == v} @-}
 (?=) :: Bool -> a -> a
 y ?= x = x
-
--- {-@ inline isBlack @-}
--- isBlack :: Tick (Tree a) -> Bool
--- isBlack t = rk (tval t) == B 
-
--- height :: Tree a -> Tree a -> Int
--- height l r = if (ht l) > (ht r) then (1 + ht l) else (1 + ht r)
-
--- -- copied from my implementation in RTick, s. commit 
--- {-@ (<$>) :: f:(a -> b) -> t1:Tick a
---                     -> { t:Tick b | Tick (tcost t1) (f (tval t1)) == t }
--- @-}
--- infixl 4 <$>
--- (<$>) :: (a -> b) -> Tick a -> Tick b
--- (<$>) = RTick.fmap 
--- -- f <$> a <*> b <*> c = Tick (a1 + a2)
-
--- {-@ reflect liftM5 @-}
--- {-@ liftM5 :: g:(a -> b -> c -> d -> e -> f) -> t1:Tick a -> t2:Tick b -> t3:Tick c -> t4:Tick d -> t5:Tick e
---                     -> { t:Tick f | g (tval t1) (tval t2) (tval t3) (tval t4) (tval t5) == tval t &&
---                       tcost t1 + tcost t2 + tcost t3 + tcost t4 + tcost t5 == tcost t } @-}
--- liftM5 :: (a -> b -> c -> d -> e -> f) -> Tick a -> Tick b -> Tick c -> Tick d -> Tick e -> Tick f
--- liftM5 f (Tick m x) (Tick n y) (Tick n1 y1) (Tick n2 y2) (Tick n3 y3) = Tick (m + n + n1 + n2 + n3) (f x y y1 y2 y3)
-
